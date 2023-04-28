4389436a35a937a95052cddf0c713f9c4df794ff
c4710n <c4710n@users.noreply.github.com>
2023-04-28 20:25:39 +0800

    chore: make it works with elixir 1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EctoIsolation.Mixfile do
     [
       app: :ecto_isolation,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases()
